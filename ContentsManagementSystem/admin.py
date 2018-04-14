0c6e253f0dc841b7c9a5b417715f25bcc69128eb
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-14 22:32:51 +0900

    Viewで書籍と感想の表示

--- ContentsManagementSystem/admin.py
+++ ContentsManagementSystem/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from ContentsManagementSystem.models import Book, Impresstion
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Impresstion)
