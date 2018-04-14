800b0bf6f5384c3cce47de5f21cd1eb136faeb01
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-14 21:49:54 +0900

    書籍モデルにコンテンツを追加

--- ContentsManagementSystem/models.py
+++ ContentsManagementSystem/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
   """書籍"""
-
+  name = models.CharField('書籍名')
+  publisher = models.CharField('出版社')
+  page = models.IntegerField('ページ数')
 
 class Impresstion(models.Model):
   """感想"""
